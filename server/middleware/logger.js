84f48a3bafc265dc87915bb3d0d19d091d123521
caolan <caolan@digigrow.ie>
2023-04-19 19:04:34 -0400

    creating a middleware for a custom http request logger file

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -2,11 +2,11 @@ const { format } = require("date-fns");
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
 const fsPromises = require("fs").promises;
-const path = require(path);
+const path = require("path");
 
 const logEvents = async (message, logFileName) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
-  const logTime = `${dateTime}\T${uuid()}\t${message}\n`;
+  const logItem = `${dateTime}\T${uuid()}\t${message}\n`;
 
   try {
     if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
@@ -22,7 +22,7 @@ const logEvents = async (message, logFileName) => {
 };
 
 const logger = (req, res, next) => {
-  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+  //logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
   console.log(`${req.method} ${req.path}`);
   next();
 };
