85e530ca8acfd1cb6e94c965df31f252d5f6669a
caolan <caolan@digigrow.ie>
2023-04-19 18:23:11 -0400

    initial setup server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
-const path = express("path");
+const path = require("path");
 const PORT = process.env.PORT || 3333;
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
