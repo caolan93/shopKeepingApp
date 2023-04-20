1064e6ec78e4a2b550ce1c08760002db1f251393
caolan <caolan@digigrow.ie>
2023-04-20 18:59:46 -0400

    update user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -65,7 +65,52 @@ const createUser = asyncHandler(async (req, res) => {
 // @route PATCH /users
 // @access Private
 const updateUser = asyncHandler(async (req, res) => {
-  const users = User.find();
+  const { id, username, password, roles, active } = req.body;
+
+  if (
+    !username ||
+    !password ||
+    !Array.isArray(roles) ||
+    !roles.length ||
+    typeof active !== "boolean"
+  ) {
+    return res.status(400).json({
+      message: "All fields must be required.",
+    });
+  }
+
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({
+      message: "User not found.",
+    });
+  }
+
+  // Check for duplicate
+  const duplicate = await User.findOne({ username }).lean().exec();
+
+  // Allow updates to the original user
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({
+      message: "This username already exists.",
+    });
+  }
+
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if (password) {
+    // Hash password
+    user.password = await bcrypt.hash(password, 10);
+  }
+
+  const updatedUser = await User.save();
+
+  return res.status(200).json({
+    message: `${updatedUser.username} updated successfully`,
+  });
 });
 
 // @desc Delete a user
