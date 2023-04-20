6671b12a9d149eb20021221a8e0c8e31c0460788
caolan <caolan@digigrow.ie>
2023-04-20 18:37:58 -0400

    create user controller

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const usersController = require("../controllers/userController");
 
-router.route("/").get().post().patch().delete();
+router
+  .route("/")
+  .get(usersController.getAllUsers)
+  .post(usersController.createUser)
+  .patch(usersController.updateUser)
+  .delete(usersController.deleteUser);
 
 module.exports = router;
